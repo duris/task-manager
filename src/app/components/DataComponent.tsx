e68ef43e6c66016a15b4eaad0d145323d3d065e0
Ross Duris <ross@duris.io>
2023-11-29 08:23:04 -0500

    json placeholder

---
+++ src/app/components/DataComponent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DataComponent = async (props: { id: number }) => {
+  return (
+    <div>
+      <h1>{JSON.stringify(res)}</h1>
+    </div>
+  );
+};
+
+export default DataComponent;
